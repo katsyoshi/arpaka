772e18eceb324b069e0419d30466f4842b77f8e4
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-09-10 17:35:14 +0900

    Handle formal argument list optional tail action

--- tool/ruby/actions.rb
+++ tool/ruby/actions.rb
@@ -118,6 +118,7 @@ module RubyGrammarActions
     412 => ["pass_through", "nil"],
     419 => ["pass_through", "nil"], 420 => ["pass_through", "nil"], 421 => ["pass_through", "nil"],
     426 => ["empty", "nil"],
+    465 => ["pass_through", "$1"],
     464 => ["pass_through", "$1"],
     463 => ["pass_through", "$1"],
     462 => ["pass_through", "$1"],
