1712a221ad9af3ad33b1a0c303c7a55abe8a4566
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-09-10 17:26:19 +0900

    Handle endless singleton method argument definition

--- tool/ruby/actions.rb
+++ tool/ruby/actions.rb
@@ -118,6 +118,7 @@ module RubyGrammarActions
     412 => ["pass_through", "nil"],
     419 => ["pass_through", "nil"], 420 => ["pass_through", "nil"], 421 => ["pass_through", "nil"],
     426 => ["empty", "nil"],
+    280 => ["pass_through", "$1"],
     279 => ["pass_through", "$1"],
     244 => ["pass_through", "$1"],
     243 => ["pass_through", "nil"],
