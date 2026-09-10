e5837fd709941ae7bddca3312a3595e9322367c4
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-09-10 17:26:19 +0900

    Handle fcall brace primary

--- tool/ruby/actions.rb
+++ tool/ruby/actions.rb
@@ -118,6 +118,7 @@ module RubyGrammarActions
     412 => ["pass_through", "nil"],
     419 => ["pass_through", "nil"], 420 => ["pass_through", "nil"], 421 => ["pass_through", "nil"],
     426 => ["empty", "nil"],
+    366 => ["pass_through", "$1"],
     356 => ["pass_through", "$2"],
     313 => ["pass_through", "$1"],
     280 => ["pass_through", "$1"],
