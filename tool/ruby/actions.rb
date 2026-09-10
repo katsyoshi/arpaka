38f71fbe93fc04050ac0065edaf1e73f1ee9f4ce
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-09-10 17:39:39 +0900

    Handle grammar action 506

--- tool/ruby/actions.rb
+++ tool/ruby/actions.rb
@@ -118,6 +118,7 @@ module RubyGrammarActions
     412 => ["pass_through", "nil"],
     419 => ["pass_through", "nil"], 420 => ["pass_through", "nil"], 421 => ["pass_through", "nil"],
     426 => ["empty", "nil"],
+    506 => ["pass_through", "$1"],
     505 => ["pass_through", "$1"],
     504 => ["pass_through", "$1"],
     503 => ["pass_through", "$1"],
