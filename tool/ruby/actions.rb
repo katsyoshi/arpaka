03d982c1ca9c58fbf903339deaf434ddb689abf1
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-09-10 17:24:04 +0900

    Handle indexed operator assignment rhs

--- tool/ruby/actions.rb
+++ tool/ruby/actions.rb
@@ -118,6 +118,7 @@ module RubyGrammarActions
     412 => ["pass_through", "nil"],
     419 => ["pass_through", "nil"], 420 => ["pass_through", "nil"], 421 => ["pass_through", "nil"],
     426 => ["empty", "nil"],
+    238 => ["pass_through", "$1"],
     237 => ["pass_through", "$1"],
     145 => ["pass_through", "$1"],
     144 => ["pass_through", "$1"],
