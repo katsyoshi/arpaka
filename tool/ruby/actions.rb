2fff4f22053cd06a8d2a6656822eec095f994604
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-09-10 17:24:04 +0900

    Handle namespace assignment lhs

--- tool/ruby/actions.rb
+++ tool/ruby/actions.rb
@@ -118,6 +118,7 @@ module RubyGrammarActions
     412 => ["pass_through", "nil"],
     419 => ["pass_through", "nil"], 420 => ["pass_through", "nil"], 421 => ["pass_through", "nil"],
     426 => ["empty", "nil"],
+    145 => ["pass_through", "$1"],
     144 => ["pass_through", "$1"],
     143 => ["pass_through", "$1"],
     142 => ["ported", "@builder.index($1, $3 || [])"],
