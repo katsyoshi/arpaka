603b7152d7bd3c2d20725d093f01c39d42762d97
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-09-10 17:24:04 +0900

    Handle method assignment lhs

--- tool/ruby/actions.rb
+++ tool/ruby/actions.rb
@@ -118,6 +118,7 @@ module RubyGrammarActions
     412 => ["pass_through", "nil"],
     419 => ["pass_through", "nil"], 420 => ["pass_through", "nil"], 421 => ["pass_through", "nil"],
     426 => ["empty", "nil"],
+    143 => ["pass_through", "$1"],
     142 => ["ported", "@builder.index($1, $3 || [])"],
     136 => ["pass_through", "$1"],
     135 => ["pass_through", "$1"],
