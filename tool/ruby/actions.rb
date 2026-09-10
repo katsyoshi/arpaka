d5ca2ddac51f224355b566ab57f8b8997f834ad1
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-09-10 17:39:39 +0900

    Handle grammar action 487

--- tool/ruby/actions.rb
+++ tool/ruby/actions.rb
@@ -118,6 +118,7 @@ module RubyGrammarActions
     412 => ["pass_through", "nil"],
     419 => ["pass_through", "nil"], 420 => ["pass_through", "nil"], 421 => ["pass_through", "nil"],
     426 => ["empty", "nil"],
+    487 => ["pass_through", "$1"],
     485 => ["pass_through", "$3"],
     481 => ["pass_through", "$2"],
     476 => ["pass_through", "$1"],
