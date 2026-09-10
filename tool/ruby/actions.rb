bec2f0df49bce5fbca6842da51f9d6a6512d7b05
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-09-10 17:26:18 +0900

    Handle backref operator assignment rhs

--- tool/ruby/actions.rb
+++ tool/ruby/actions.rb
@@ -118,6 +118,7 @@ module RubyGrammarActions
     412 => ["pass_through", "nil"],
     419 => ["pass_through", "nil"], 420 => ["pass_through", "nil"], 421 => ["pass_through", "nil"],
     426 => ["empty", "nil"],
+    244 => ["pass_through", "$1"],
     243 => ["pass_through", "nil"],
     242 => ["pass_through", "$1"],
     241 => ["pass_through", "$1"],
