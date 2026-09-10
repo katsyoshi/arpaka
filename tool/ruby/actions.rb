0dc2db2182424e180437535d3d4e415135cd4414
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-09-10 17:24:04 +0900

    Handle constant operator assignment rhs

--- tool/ruby/actions.rb
+++ tool/ruby/actions.rb
@@ -118,6 +118,7 @@ module RubyGrammarActions
     412 => ["pass_through", "nil"],
     419 => ["pass_through", "nil"], 420 => ["pass_through", "nil"], 421 => ["pass_through", "nil"],
     426 => ["empty", "nil"],
+    240 => ["pass_through", "$1"],
     239 => ["pass_through", "$1"],
     238 => ["pass_through", "$1"],
     237 => ["pass_through", "$1"],
