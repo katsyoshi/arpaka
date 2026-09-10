5847934ded3a23e6ad095955b1ac5f45fa075b1e
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-09-10 17:35:14 +0900

    Handle kwarg list action

--- tool/ruby/actions.rb
+++ tool/ruby/actions.rb
@@ -118,6 +118,7 @@ module RubyGrammarActions
     412 => ["pass_through", "nil"],
     419 => ["pass_through", "nil"], 420 => ["pass_through", "nil"], 421 => ["pass_through", "nil"],
     426 => ["empty", "nil"],
+    448 => ["pass_through", "$1"],
     445 => ["pass_through", "$1"],
     368 => ["pass_through", "$1"],
     366 => ["pass_through", "$1"],
