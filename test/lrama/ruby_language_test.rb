f8a710b6e884bad6533f4fedeac674ebc9ff4d95
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-09-12 18:51:22 +0900

    Recognize modifiers after block endings

--- test/lrama/ruby_language_test.rb
+++ test/lrama/ruby_language_test.rb
@@ -536,6 +536,12 @@ class ArpakaTest < Test::Unit::TestCase
     assert_nothing_raised { Arpaka.parse_source("def stop\n  return unless value\nend\n") }
   end
 
+  test "source lexer recognizes modifiers after block endings" do
+    tokens = Arpaka.const_get(:Lexer, false).new("foo do\nend unless value\n").each.to_a
+    assert_equal(:modifier_unless, tokens[5].first)
+    assert_nothing_raised { Arpaka.parse_source("foo do\nend unless value\n") }
+  end
+
   test "source lexer accepts escaped newlines between expressions" do
     tokens = Arpaka.const_get(:Lexer, false).new("\"left\" \\\n\"right\"\n").each.to_a
     assert_equal([:tSTRING_BEG, :tSTRING_CONTENT, :tSTRING_END, :tSTRING_BEG], tokens.map(&:first)[0, 4])
